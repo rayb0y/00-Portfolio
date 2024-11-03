a907544a11097e5be85f7b41c91fb23c9398a285
rayb0y <amalray.ray65@gmail.com>
2024-11-03 19:36:06 +0530

    deleted:    Website/app.js 	deleted:    Website/colors.txt 	deleted:    Website/index.html 	deleted:    Website/styles.css 	deleted:    Website/tech.html

--- Website/app.js
+++
@@ -1,7 +0,0 @@
-const mobileMenuButton = document.getElementById("mobile-menu");
-const navbarMenu = document.querySelector(".navbar__menu");
-
-mobileMenuButton.addEventListener("click", () => {
-  navbarMenu.classList.toggle("active");
-  mobileMenuButton.classList.toggle("is-active");
-});
